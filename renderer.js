8ae566c56511093ec3e7b903a6d180de113575df
DelBianco <andredbg@gmail.com>
2019-02-17 23:04:05 -0300

    Adicionando tabela de dados e gráfico

--- renderer.js
+++ renderer.js
@@ -1,46 +1,129 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
 window.$ = window.jQuery = require('jquery');
 window.Tether = require('tether');
 window.Bootstrap = require('bootstrap');
 
 const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const Chart = require('chart.js');
+const parser = new Readline();
+
+let com = null;
+let ports = [];
+
 let buffer = {
   data: [],
   size: 6,
 };
 
 buffer.data.push = function() {
-  if(this.length > buffer.size){
+  if (this.length > buffer.size) {
     this.shift();
   }
-
-  document.getElementById('debug').textContent = this.join();
-  return Array.prototype.push.apply(this,arguments);
+  return Array.prototype.push.apply(this, arguments);
 };
 
-
-SerialPort.list((err, ports) => {
-  if (err) {
-    document.getElementById('error').textContent = err.message
-    return
-  } else {
-    document.getElementById('error').textContent = ''
+let chart = new Chart($("#chart"), {
+  type: 'line',
+  data: {
+    datasets: []
   }
 });
 
-const port = new SerialPort('/dev/ttyUSB0');
+let dataBuffer = {
+  elem: $('#table-body'),
+  data: [],
+  size: 60,
+  add: function(line) {
+    this.data.push(line);
+    let markup = "<tr>";
+    line.forEach(function(cel,index) {
+      if(chart.data.datasets[index] === undefined){
+        chart.data.datasets.push(
+            {data : []}
+        );
+      }
+      chart.data.datasets[index].data.push(cel);
+      markup += "<td>" + cel+ "</td>";
+    });
+    markup += "</tr>";
+    this.elem.append(markup);
+    if(this.elem.children().length > this.size){
+      this.elem.children().first().remove();
+      chart.data.datasets.forEach((dataset) => {
+        dataset.data.shift();
+      });
+    }
+    chart.update();
+  }
+};
 
-// The open event is always emitted
-port.on('open', function(port) {
-  console.log(port)
-})
+dataBuffer.data.push = function() {
+  if (this.length > dataBuffer.size) {
+    this.shift();
+  }
+  return Array.prototype.push.apply(this, arguments);
+};
 
-// The open event is always emitted
-port.on('data', function(data) {
-  buffer.data.push(data);
-})
 
+
+let process = {
+  input: function(data) {
+    let val = data.toString().split(",");
+    switch (val[0]) {
+      case "MSG":
+        $("#msg").html(val[1]);
+        break;
+      case "DATA":
+        val.shift();
+        dataBuffer.add(val);
+    }
+  }
+};
+
+
+let error = {
+  set: function(data) {
+    $('#error').html(data);
+  }
+};
+
+SerialPort.list((err, ps) => {
+  ports = ps;
+  if (err) {
+    $('#error').html(err.message);
+  } else {
+    $('#error').html('');
+    ports.forEach( (p , index) => {
+      if(! (p.productId === undefined)){
+        let option = new Option(p.comName, index);
+        $('#port').append($(option));
+      }
+    });
+  }
+});
+
+$('#port').change(function() {
+  com = ports[$( this ).val()];
+});
+
+$('#connect').on('click', function() {
+
+  if (! (com === null)) {
+    const port = new SerialPort(com.comName);
+    port.pipe(parser);
+    // The open event is always emitted
+    port.on('open', function(port) {
+      //console.log(port);
+    });
+
+    // The open event is always emitted
+    parser.on('data', function(data) {
+      buffer.data.push(data);
+      process.input(data);
+    });
+
+  } else {
+    error.set('Selecione uma porta');
+  }
+});
 
