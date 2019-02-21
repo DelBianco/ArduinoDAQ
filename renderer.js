840951adbd4661b52d3a9af778f2fb1a38ac4225
DelBianco <andredbg@gmail.com>
2019-02-20 22:48:56 -0300

    Adicionando tabela de dados e gráfico

--- renderer.js
+++ renderer.js
@@ -4,15 +4,33 @@ window.Bootstrap = require('bootstrap');
 
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
-const Chart = require('chart.js');
 const parser = new Readline();
+
+let arduinoDAQ = {
+  series: [],
+  labels: [],
+  data: [],
+  msg: null,
+  datasize: 100,
+  refreshRate: 1000,
+  render: function() {
+    $('#msg').html(this.msg);
+  },
+};
+
+arduinoDAQ.data.push = function() {
+  if (this.length > arduinoDAQ.datasize) {
+    this.shift();
+  }
+  return Array.prototype.push.apply(this, arguments);
+};
 
 let com = null;
 let ports = [];
 
 let buffer = {
   data: [],
-  size: 6,
+  size: 20,
 };
 
 buffer.data.push = function() {
@@ -22,69 +40,27 @@ buffer.data.push = function() {
   return Array.prototype.push.apply(this, arguments);
 };
 
-let chart = new Chart($("#chart"), {
-  type: 'line',
-  data: {
-    datasets: []
-  }
-});
-
-let dataBuffer = {
-  elem: $('#table-body'),
-  data: [],
-  size: 60,
-  add: function(line) {
-    this.data.push(line);
-    let markup = "<tr>";
-    line.forEach(function(cel,index) {
-      if(chart.data.datasets[index] === undefined){
-        chart.data.datasets.push(
-            {data : []}
-        );
-      }
-      chart.data.datasets[index].data.push(cel);
-      markup += "<td>" + cel+ "</td>";
-    });
-    markup += "</tr>";
-    this.elem.append(markup);
-    if(this.elem.children().length > this.size){
-      this.elem.children().first().remove();
-      chart.data.datasets.forEach((dataset) => {
-        dataset.data.shift();
-      });
-    }
-    chart.update();
-  }
-};
-
-dataBuffer.data.push = function() {
-  if (this.length > dataBuffer.size) {
-    this.shift();
-  }
-  return Array.prototype.push.apply(this, arguments);
-};
-
-
-
 let process = {
   input: function(data) {
-    let val = data.toString().split(",");
+    let val = data.toString().split(',');
     switch (val[0]) {
-      case "MSG":
-        $("#msg").html(val[1]);
+      case 'MSG':
+        arduinoDAQ.msg = val[1];
         break;
-      case "DATA":
+      case 'DATA':
         val.shift();
-        dataBuffer.add(val);
+        arduinoDAQ.data.push(val);
+        arduinoDAQ.labels.push(val[0]);
+        val.shift();
+        arduinoDAQ.series.push(val);
     }
-  }
+  },
 };
-
 
 let error = {
   set: function(data) {
     $('#error').html(data);
-  }
+  },
 };
 
 SerialPort.list((err, ps) => {
@@ -93,8 +69,8 @@ SerialPort.list((err, ps) => {
     $('#error').html(err.message);
   } else {
     $('#error').html('');
-    ports.forEach( (p , index) => {
-      if(! (p.productId === undefined)){
+    ports.forEach((p, index) => {
+      if (!(p.productId === undefined)) {
         let option = new Option(p.comName, index);
         $('#port').append($(option));
       }
@@ -103,27 +79,26 @@ SerialPort.list((err, ps) => {
 });
 
 $('#port').change(function() {
-  com = ports[$( this ).val()];
+  com = ports[$(this).val()];
 });
 
 $('#connect').on('click', function() {
-
-  if (! (com === null)) {
+  if (!(com === null)) {
     const port = new SerialPort(com.comName);
     port.pipe(parser);
-    // The open event is always emitted
-    port.on('open', function(port) {
-      //console.log(port);
-    });
-
     // The open event is always emitted
     parser.on('data', function(data) {
       buffer.data.push(data);
       process.input(data);
     });
 
+    setInterval(function() {
+      arduinoDAQ.render();
+    },arduinoDAQ.refreshRate);
+
   } else {
     error.set('Selecione uma porta');
   }
-});
 
+
+});
